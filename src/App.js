6b2879eb5856f34185ea25114d2b308f534be76a
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-27 17:42:38 +0300

    move main nav title to the props
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainNav />
+        <MainNav title="Start Bootstrap"/>
 
     {/*<!-- Page Content -->*/}
     <div class="container">
