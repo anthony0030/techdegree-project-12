0db195811108a122af9776a8ff6a135c62e2b8b0
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-27 17:50:21 +0300

    move projects list to seperate component
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 // Application Components
 import MainNav from "./components/MainNav"
-import Project from "./components/Project"
+import Projects from "./components/Projects"
 import Heading from "./components/Heading"
 import Pagination from "./components/Pagination"
 import Footer from "./components/Footer"
@@ -26,54 +26,7 @@ class App extends Component {
       {/*<!-- Page Heading -->*/}
       <Heading title="Page Heading" subtitle="Secondary Text"/>
 
-      <div class="row">
-
-
-        <Project
-          title="Project One"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!"
-        />
-
-
-        <Project
-          title="Project Two"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-        />
-
-
-        <Project
-          title="Project Three"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!"
-        />
-
-
-        <Project
-          title="Project Four"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-        />
-
-
-        <Project 
-          title="Project Five"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-        />
-
-
-        <Project
-          title="Project Six"
-          photo="http://placehold.it/700x400"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse."
-        />
-
-
-
-      </div> 
-      {/*//.row*/}
+      <Projects />
 
       <Pagination />
 
