95a9ddeddb3ff8942b91d599cf87fdaab99a17ca
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-27 17:24:20 +0300

    move pagination to seperate file
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import MainNav from "./components/MainNav"
 import Project from "./components/Project"
 import Heading from "./components/Heading"
+import Pagination from "./components/Pagination"
 import Footer from "./components/Footer"
 
 
@@ -75,29 +76,7 @@ class App extends Component {
       {/*<!-- /.row -->*/}
 
       {/*<!-- Pagination -->*/}
-      <ul class="pagination justify-content-center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
+      <Pagination />
 
     </div>
     {/*<!-- /.container -->*/}
---
+++ src/components/Pagination.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Pagination() {
+  return (
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">1</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">2</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">3</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+
+
+Pagination.propTypes = {
+  // title: PropTypes.string.isRequired,
+  // photo: PropTypes.string.isRequired,
+  // description: PropTypes.string.isRequired
+};
+
+
+export default Pagination;
