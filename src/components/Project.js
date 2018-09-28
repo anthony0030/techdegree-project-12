d4005b550585fc51870c9029c0bcfd0c9a19b55a
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-28 13:45:26 +0300

    display project preview or image based on screen size
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,9 +6,8 @@ function Project(props) {
   return (
     <div className="col-lg-4 col-sm-6 portfolio-item">
       <div className="card h-100">
-{/*        <a href={props.url}><img className="card-img-top" src={props.photo} alt="" /></a>*/}
-<iframe height="450" className="card-img-top" title={props.title} src={props.url}></iframe>
-
+        <a href={props.url}><img className="card-img-top d-md-none d-xl-none" src={props.photo} alt="" /></a>
+        <iframe height="450" className="card-img-top d-none d-md-block" title={props.title} src={props.url} />
         <div className="card-body">
           <h4 className="card-title">
             <a href={props.url}>{props.title}</a>
