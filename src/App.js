eb47b4459a18f9c7637192b23728eba1dbe9dfd1
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-28 12:29:54 +0300

    add real project information
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -21,50 +21,98 @@ const PROJECTS = [
   {
     title: "Project One",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
+    url: " https://anthony0030.github.io/techdegree-project-01",
     codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
     codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!"
+    description: "PROJECT DESCRIPTION HERE!!!!"
   },
   {
     title: "Project Two",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
-    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
-    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.",
+        url: " https://anthony0030.github.io/techdegree-project-02",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-02",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/f4cb99b5c0ee48ab8ca4adceb8f5aa51",
+    description: "PROJECT DESCRIPTION HERE!!!!"
   },
   {
     title: "Project Three",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
-    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
-    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!"
+        url: " https://anthony0030.github.io/techdegree-project-03",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-03",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/f9c69f0b12f840709f96a1b8e7beba07",
+    description: "PROJECT DESCRIPTION HERE!!!!"
   },
   {
     title: "Project Four",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
-    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
-    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
+        url: " https://anthony0030.github.io/techdegree-project-04",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-04",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/0b84447f14204f728afd550ad62f1a96",
+    description: "PROJECT DESCRIPTION HERE!!!!"
   },
   { 
     title: "Project Five",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
-    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
-    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
+        url: " https://anthony0030.github.io/techdegree-project-05",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-05",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/19a44b27eef448da9443ab3664cf64ab",
+    description: "PROJECT DESCRIPTION HERE!!!!"
   },
   {
     title: "Project Six",
     photo: "http://placehold.it/700x400",
-    url: "http://placehold.it/700x400",
-    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
-    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse."
+        url: " https://anthony0030.github.io/techdegree-project-06",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-06",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/d658f87a7c084292b2ec59adfd35c5f8",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project Seven",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-07",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-07",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/8ed9891faa9e4170a6d7694fabe70aec",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project Eight",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-08",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-08",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/cb18dfce7f394a33ac8851cb8f4899ba",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project Nine",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-09",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-09",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/c21087afd9644182b32ae612d47f5240",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project Ten",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-10",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-10",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/d55409a80c6543f8bb7b9659afd19852",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project Eleven",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-11",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-11",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/630d8b9691f349d5bd6f37c534a23498",
+    description: "PROJECT DESCRIPTION HERE!!!!"
+  },
+  {
+    title: "Project twelve",
+    photo: "http://placehold.it/700x400",
+        url: " https://anthony0030.github.io/techdegree-project-12",
+    codayBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-12",
+    codayBadgeIcon: "https://api.codacy.com/project/badge/Grade/933fdff7981d4d92894f2ffafb2f00b9",
+    description: "PROJECT DESCRIPTION HERE!!!!"
   }
 ]
 
