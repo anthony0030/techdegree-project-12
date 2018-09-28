34a654a77d1c905eaa27da47942e45771425c1bf
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-28 11:25:33 +0300

    make the company name in main nav go to the home page
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -8,7 +8,7 @@ function MainNav(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
-        <a className="navbar-brand" href="#">{props.title}</a>
+        <a className="navbar-brand" href="/home">{props.title}</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
