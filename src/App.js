ae9e1cb3071981de86c7ae13fc04574f537bb343
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-28 11:18:05 +0300

    print projects programaticaly
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,6 +15,46 @@ import Footer from "./components/Footer"
 
 import E404 from "./components/E404";
 
+
+
+const PROJECTS = [
+  {
+    title: "Project One",
+    photo: "http://placehold.it/700x400",
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!"
+  },
+  {
+    title: "Project Two",
+    photo: "http://placehold.it/700x400",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.",
+  },
+  {
+    title: "Project Three",
+    photo: "http://placehold.it/700x400",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!"
+  },
+  {
+    title: "Project Four",
+    photo: "http://placehold.it/700x400",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
+  },
+  { 
+    title: "Project Five",
+    photo: "http://placehold.it/700x400",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
+  },
+  {
+    title: "Project Six",
+    photo: "http://placehold.it/700x400",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse."
+  }
+]
+
+
+
+
+
+
 class App extends Component {
   render() {
     return (
@@ -25,7 +65,7 @@ class App extends Component {
             <Heading title="Page Heading" subtitle="Secondary Text"/>
             <Switch>
               <Redirect exact from="/" to="/home"/>
-              <Route exact path="/home" component={Projects} />
+              <Route exact path="/home" render={ () => <Projects data={PROJECTS} /> } />
               <Route exact path="/about" component={E404} />
               <Route exact path="/Services" component={E404} />
               <Route exact path="/Contact" component={E404} />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,50 +9,23 @@ import Project from "./Project"
 function Projects(props) {
   return (
     <div class="row">
-
-      <Project
-        title="Project One"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!"
-      />
-
-      <Project
-        title="Project Two"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-      />
-
-      <Project
-        title="Project Three"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!"
-      />
-
-      <Project
-        title="Project Four"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-      />
-
-      <Project 
-        title="Project Five"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae."
-      />
-
-      <Project
-        title="Project Six"
-        photo="http://placehold.it/700x400"
-        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse."
-      />
-
+      {
+        props.data.map(
+          (project, index) =>
+            <Project
+              key={index}
+              title={project.title}
+              photo={project.photo}
+              description={project.description}
+            />
+          )
+        }
     </div>
   );
 }
 
 Projects.propTypes = {
-  // title: PropTypes.string.isRequired,
-  // subtitle: PropTypes.string,
+  data: PropTypes.array.isRequired
 };
 
 
