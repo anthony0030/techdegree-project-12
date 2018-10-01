7e6cf3eaacf60aa12573c1947181447ee24c5498
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-10-01 11:21:04 +0300

    add the metadata to my contact card
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,11 +17,11 @@ function Contact(props) {
           <div className="card-body">
             <ul className="list-group">
 
-              <ContactLine icon="person" text="Anthony Veaudry" link="#"/>
-              <ContactLine icon="person" text="anthony0030" link="#"/>
-              <ContactLine icon="location_on" text="Greece" link="#"/>
-              <ContactLine icon="email" text="anthony@veaudry.pro" link="#"/>
-              <ContactLine icon="phone" text="+30 6948688704" link="#"/>
+              <ContactLine icon="person" text="Anthony Veaudry" link="https://www.facebook.com/anthonyveaudry"/>
+              <ContactLine icon="person" text="anthony0030" link="https://github.com/anthony0030"/>
+              <ContactLine icon="location_on" text="Greece" link="https://wikipedia.org/wiki/Greece"/>
+              <ContactLine icon="email" text="anthony@veaudry.pro" link="mailto:anthony@veaudry.pro"/>
+              <ContactLine icon="phone" text="+30 6948688704" link="tel:+30 6948688704"/>
 
             </ul>
           </div>
