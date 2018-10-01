9a9e63a507cb93629701876e7507aae5724aa923
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-10-01 11:15:10 +0300

    ad a class name prop to my icons
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 
 function Icon(props) {
   return (
-   <i className="material-icons contactIcon">{props.i}</i>
+   <i className={`material-icons ${props.className}`}>{props.i}</i>
   );
 }
 
 Icon.propTypes = {
-  i: PropTypes.string.isRequired
+  i: PropTypes.string.isRequired,
+  className: PropTypes.string
 };
 
 
