507b4ffeb6dde4a5b4384276d78e1957d63a51c8
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-10-01 13:05:37 +0300

    make the footer smaller
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Footer(props) {
   return (
-    <footer className="py-5 bg-dark fixed-bottom">
+    <footer className="py-1 bg-dark fixed-bottom">
       <div className="container">
         <a href={props.href}>
           <p className="m-0 text-center text-white">{props.text}</p>
