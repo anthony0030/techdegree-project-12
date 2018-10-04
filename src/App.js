0718cc44f9c86d387de5830111c1815a9a92614d
Anthony Veaudry <anthony@veaudry.pro>
2018-10-04 16:39:44 +0300

    add a better description to the projects
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass" , "jquery", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-01",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/5ca7a91d6de340abbe125cae4961e50c",
-    description: "Customize a personal online profile by adding graphics, customizing text, and improving the look of a web page using Cascading Style Sheets, the formatting language of the web. Share your finished project on GitHub, a website used by teams of programmers to share and work on programming code."
+    description: "In this project, I was given a template to fill my details into. I converted the template into a JADE and the styling to SASS. I then proceeded to add a photo a custom photo gallery. I used the built-in bower functionality of CodeKit to manage the dependencies."
   },
   {
     title: "Responsive Site",
@@ -52,7 +52,7 @@ const PROJECTS = [
     skills: ["html", "css", "jade", "sass"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-02",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/f4cb99b5c0ee48ab8ca4adceb8f5aa51",
-    description: "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays."
+    description: "In this project, I was given a mock-up for a responsive website. I made the website using JADE and SASS."
   },
   {
     title: "Registration Form",
@@ -62,7 +62,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "jquery", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-03",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/f9c69f0b12f840709f96a1b8e7beba07",
-    description: "Web forms appear everywhere online: forms allow users to order books, sign up for web sites, and post to Facebook. This project challenges you to build a responsive, mobile-first registration form using a variety of HTML form elements."
+    description: "In this project, I was challenged to build a responsive, mobile-first registration form using a variety of HTML form elements. I built it using JADE and SASS. I also added a bunch of plug-ins for form validation."
   },
   {
     title: "Photo Gallery",
@@ -72,7 +72,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "jquery", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-04",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/0b84447f14204f728afd550ad62f1a96",
-    description: "Interactive image galleries are a common feature of many websites, from photo sites to e-commerce applications. Use HTML, CSS and the popular programming language JavaScript to create an interactive, searchable gallery of photos."
+    description: "In this project, I was excited to build a searchable photo gallery. I used JADE and SASS. It also uses a mobile-first design."
   },
   { 
     title: "Web Style Guide",
@@ -82,7 +82,7 @@ const PROJECTS = [
     skills: ["css" , "sass", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-05",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/19a44b27eef448da9443ab3664cf64ab",
-    description: "Sass is an important tool in a modern Front End Web Developer’s toolbox. It's used by many developers to make styling web pages with CSS easier and faster. Create a style guide that can act as your own personal and custom Bootstrap that you can drop into any of your projects to speed up styling, layout and development in general."
+    description: "In this project, I was challenged to build a framework like bootstrap from scratch using SASS. It was great splitting the styling into multiple files that compile into one tiny CSS file."
   },
   {
     title: "Video Player",
@@ -92,7 +92,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "jquery", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-06",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/d658f87a7c084292b2ec59adfd35c5f8",
-    description: "Build an HTML5 video player using JavaScript and the HTML5 Video API. Video, and the interface used to control the video, is increasingly important for many different kinds of web applications, including web site landing pages, blogs, podcasts, product marketing and training."
+    description: "In this project, I Built a video player with closed captions beneath it. The captions are interactive so they highlight when the video is playing and you can click on the text to take you to that part of the video."
   },
   {
     title: "Game Show App",
@@ -102,7 +102,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "jquery", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-07",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/8ed9891faa9e4170a6d7694fabe70aec",
-    description: "In this project, you'll create a browser version of “Wheel of Success”, a word guessing game. You’ll use Javascript to come up with a random phrase that players will try to guess by entering different letters into the program."
+    description: "In this project, I created a browser version of “Wheel of Success”, a word guessing game. I used Javascript to come up with a random phrase that players try to guess by entering different letters into the program."
   },
   {
     title: "SVG Animations",
@@ -112,7 +112,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "svg"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-08",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/cb18dfce7f394a33ac8851cb8f4899ba",
-    description: "Master Scaleable Vector Graphics. SVG graphics are quickly becoming one of the most widely used graphic formats on the web. Modernize the user interface of a web page by adding, styling and optimizing SVG graphics."
+    description: "In this project, I master Scaleable Vector Graphics. I modernized the user interface of a web page by adding, styling, optimizing and animating SVG graphics."
   },
   {
     title: "Web Dashboard",
@@ -122,7 +122,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "svg", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-09",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/c21087afd9644182b32ae612d47f5240",
-    description: "Build an interactive dashboard for a web application using advanced web techniques including SVG graphics and JavaScript programming. The project involves creating tables, charts, graphics and other user interface components in a manner that promotes interactivity and usability."
+    description: "In this project, I built an interactive dashboard for a web application using advanced web techniques including SVG graphics and JavaScript programming. The project involved creating tables, charts, graphics and other user interface components in a manner that promotes interactivity and usability. I utilized my knowledge of SCSS and JADE to build this project faster"
   },
   {
     title: "Using APIs",
@@ -132,7 +132,7 @@ const PROJECTS = [
     skills: ["html", "css", "js" ,"jade", "sass", "svg", "api", "bower"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-10",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/d55409a80c6543f8bb7b9659afd19852",
-    description: "Many sites — Twitter, Facebook, IMDB, and Wikipedia to name a few — offer a vast sea of data that you can access and display on your own web pages. Using JavaScript, you’ll create an employee directory by communicating with a third-party API (Application Programming Interface)."
+    description: "In this project, I tapped into an API to render a page with random user data to make an 'employee directory' that populates with random user data."
   },
   {
     title: "React Gallery",
@@ -142,7 +142,7 @@ const PROJECTS = [
     skills: ["html", "css", "js", "api", "react"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-11",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/630d8b9691f349d5bd6f37c534a23498",
-    description: "Create an image gallery using React and the Flickr API. You will build the gallery components, add to the supplied CSS and set up routing."
+    description: "In this project, I created an image gallery using React and the Flickr API. I build the gallery components, added the supplied CSS and set up routing."
   },
   {
     title: "Online Portfolio",
@@ -152,7 +152,7 @@ const PROJECTS = [
     skills: ["html", "css", "js", "bootstrap", "react", "jquery", "svg"],
     codacyBadgeLink: "https://www.codacy.com/app/anthony0030/techdegree-project-12",
     codacyBadgeIcon: "https://api.codacy.com/project/badge/Grade/933fdff7981d4d92894f2ffafb2f00b9",
-    description: "The final capstone project for the Front End Web Development Techdegree Program. Show off your skill and expertise in the tools and techniques you've learned in the Techdegree by building a professional portfolio to showcase your work."
+    description: "In this project, I AM Show off MY skill and expertise in the tools and techniques I have learned in the Treehouse Techdegree by building a professional portfolio to showcase my work."
   }
 ]
 
