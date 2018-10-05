d24c778983f0e2dfcf1e126ebf377b0bb71395e1
Anthony Veaudry <anthony@veaudry.pro>
2018-10-05 16:12:25 +0300

    remove footer from main app
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-
-// Application Components
-import Footer from "./components/Footer"
-
 //Aplication Pages
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
