ead8a8302ab266e57d4236f2ed309efc2c2f0505
Anthony Veaudry <anthony@veaudry.pro>
2018-10-12 17:06:22 +0300

    fix the padding of the framer
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/components/Framer.js
+++ src/components/Framer.js
@@ -8,7 +8,8 @@ const customStyles = {
     top                   : "0",
     left                  : "0",
     right                 : "0",
-    bottom                : "0"
+    bottom                : "0",
+    padding               : "0"
   },
 
   overlay: {
