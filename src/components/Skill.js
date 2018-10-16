0cc4e1a18553a5d678741b25444ea74bfc5561d1
Anthony Veaudry <anthony@veaudry.pro>
2018-10-16 17:13:43 +0300

    make skill tooltips personal
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -31,85 +31,85 @@ function Skill(props) {
 
     case "html":
       icon = htmlBadge;
-      toolTip= "HTML was used in this project";
+      toolTip= "I used HTML in this project!";
       alt = "HTML Badge Icon";
     break;
 
     case "css":
       icon = cssBadge;
-      toolTip= "CSS was used in this project";
+      toolTip= "I used CSS in this project!";
       alt = "CSS Badge Icon";
     break;
 
     case "js":
       icon = jsBadge;
-      toolTip= "JavaScript was used in this project";
+      toolTip= "I used JavaScript in this project!";
       alt = "JS Badge Icon";
     break;
 
     case "sass":
       icon = sassBadge;
-      toolTip= "SASS was used in this project";
+      toolTip= "I used SASS in this project!";
       alt = "SASS Badge Icon";
     break;
 
     case "jade":
       icon = jadeBadge;
-      toolTip= "JADE was used in this project";
+      toolTip= "I used JADE in this project!";
       alt = "JADE Badge Icon";
     break;
 
     case "react":
       icon = reactBadge;
-      toolTip= "REACT was used in this project";
+      toolTip= "I used REACT in this project!";
       alt = "REACT Badge Icon";
     break;
 
     case "angular":
       icon = angularBadge;
-      toolTip= "Angular was used in this project";
+      toolTip= "I used Angular in this project!";
       alt = "Angular Badge Icon";
     break;
 
     case "cookie":
       icon = cookieBadge;
-      toolTip= "This project makes use of cookies";
+      toolTip= "I used cookies in this project!";
       alt = "Cookie Badge Icon";
     break;
 
    case "api":
       icon = apiBadge;
-      toolTip= "This project makes use of an API";
+      toolTip= "I used an API in this project!";
       alt = "API Badge Icon";
     break;
 
      case "bootstrap":
       icon = bootstrapBadge;
-      toolTip= "This project makes use of Bootstrap";
+      toolTip= "I used Bootstrap in this project!";
       alt = "bootstrap Badge Icon";
     break;
 
      case "jquery":
       icon = jqueryBadge;
-      toolTip= "This project makes use of jQuery";
+      toolTip= "I used jQuery in this project!";
       alt = "jQuery Badge Icon";
     break;
 
      case "npm":
       icon = npmBadge;
-      toolTip= "This project makes use of npm";
+      toolTip= "I used npm in this project!";
       alt = "npm Badge Icon";
     break;
 
      case "bower":
       icon = bowerBadge;
-      toolTip= "This project makes use of Bower";
+      toolTip= "I used Bower in this project!";
       alt = "bower Badge Icon";
     break;
 
    case "svg":
       icon = svgBadge;
-      toolTip= "This project makes use of SVGs";
+      toolTip= "I used SVGs in this project!";
       alt = "svg Badge Icon";
     break;
 
