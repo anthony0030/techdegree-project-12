81cf61db0fce92ea20ac4f3ad6be74376e227ceb
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-05 18:10:41 +0200

    simplify skill code
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -3,24 +3,24 @@ import PropTypes from "prop-types";
 
 
 // Application Badges
-import androidBadge from "./Badges/android.png";
-import apacheBadge from "./Badges/apache.png";
+import android from "./Badges/android.png";
+import apache from "./Badges/apache.png";
 import appcelerator from "./Badges/appcelerator.png";
-import htmlBadge from "./Badges/html.png";
-import cssBadge from "./Badges/css.png";
-import jsBadge from "./Badges/js.png";
-import sassBadge from "./Badges/sass.png";
-import jadeBadge from "./Badges/jade.png";
-import reactBadge from "./Badges/react.png";
-import angularBadge from "./Badges/angular.png";
-import cookieBadge from "./Badges/cookie.png";
-import bootstrapBadge from "./Badges/bootstrap.png";
-import jqueryBadge from "./Badges/jquery.png";
-import apiBadge from "./Badges/api.png";
-import errorBadge from "./Badges/error.png";
-import npmBadge from "./Badges/npm.png";
-import bowerBadge from "./Badges/bower.png";
-import svgBadge from "./Badges/svg.png";
+import html from "./Badges/html.png";
+import css from "./Badges/css.png";
+import js from "./Badges/js.png";
+import sass from "./Badges/sass.png";
+import jade from "./Badges/jade.png";
+import react from "./Badges/react.png";
+import angular from "./Badges/angular.png";
+import cookie from "./Badges/cookie.png";
+import bootstrap from "./Badges/bootstrap.png";
+import jquery from "./Badges/jquery.png";
+import api from "./Badges/api.png";
+import error from "./Badges/error.png";
+import npm from "./Badges/npm.png";
+import bower from "./Badges/bower.png";
+import svg from "./Badges/svg.png";
 
 
 let icon;
@@ -33,97 +33,97 @@ function Skill(props) {
   switch(props.skill) {
 
     case "html":
-      icon = htmlBadge;
+      icon = html;
       toolTip= "I used HTML in this project!";
       alt = "HTML Badge Icon";
     break;
 
     case "css":
-      icon = cssBadge;
+      icon = css;
       toolTip= "I used CSS in this project!";
       alt = "CSS Badge Icon";
     break;
 
     case "js":
-      icon = jsBadge;
+      icon = js;
       toolTip= "I used JavaScript in this project!";
       alt = "JS Badge Icon";
     break;
 
     case "sass":
-      icon = sassBadge;
+      icon = sass;
       toolTip= "I used SASS in this project!";
       alt = "SASS Badge Icon";
     break;
 
     case "jade":
-      icon = jadeBadge;
+      icon = jade;
       toolTip= "I used JADE in this project!";
       alt = "JADE Badge Icon";
     break;
 
     case "react":
-      icon = reactBadge;
+      icon = react;
       toolTip= "I used REACT in this project!";
       alt = "REACT Badge Icon";
     break;
 
     case "angular":
-      icon = angularBadge;
+      icon = angular;
       toolTip= "I used Angular in this project!";
       alt = "Angular Badge Icon";
     break;
 
     case "cookie":
-      icon = cookieBadge;
+      icon = cookie;
       toolTip= "I used cookies in this project!";
       alt = "Cookie Badge Icon";
     break;
 
    case "api":
-      icon = apiBadge;
+      icon = api;
       toolTip= "I used an API in this project!";
       alt = "API Badge Icon";
     break;
 
      case "bootstrap":
-      icon = bootstrapBadge;
+      icon = bootstrap;
       toolTip= "I used Bootstrap in this project!";
       alt = "bootstrap Badge Icon";
     break;
 
      case "jquery":
-      icon = jqueryBadge;
+      icon = jquery;
       toolTip= "I used jQuery in this project!";
       alt = "jQuery Badge Icon";
     break;
 
      case "npm":
-      icon = npmBadge;
+      icon = npm;
       toolTip= "I used npm in this project!";
       alt = "npm Badge Icon";
     break;
 
      case "bower":
-      icon = bowerBadge;
+      icon = bower;
       toolTip= "I used Bower in this project!";
       alt = "bower Badge Icon";
     break;
 
    case "svg":
-      icon = svgBadge;
+      icon = svg;
       toolTip= "I used SVGs in this project!";
       alt = "svg Badge Icon";
     break;
 
     case "android":
-      icon = androidBadge;
+      icon = android;
       toolTip= "";
       alt = "android Badge Icon";
     break;
 
     case "apache":
-      icon = apacheBadge;
+      icon = apache;
       toolTip= "";
       alt = "apache Badge Icon";
     break;
@@ -135,7 +135,7 @@ function Skill(props) {
     break;
 
     default:
-      icon = errorBadge;
+      icon = error;
       toolTip= "Failed to load icon or icon does not exist";
       alt = "Error with Icon";
   }
