ff58c1c5ec9b456f2500f623cae8512504d683b3
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-06 13:48:32 +0200

    make the 404 page nicer
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/pages/E404.js
+++ src/components/pages/E404.js
@@ -3,12 +3,21 @@ import IMG404 from "../404.jpg";
 
 function E404() {
   return (
-    <ul>
-      <li className="not-found">
-        <h1>404 someone took this page</h1>
-        <img src={IMG404} alt="404 Page not found" className="page-not-found"/>
-      </li>
-    </ul>
+    <React.Fragment>
+      <h1>404 someone took this page</h1>
+      <style>
+        {`
+          body{
+            background-image: url(${IMG404});
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            background-color: #fff;
+          }
+        `}
+      </style>
+    </React.Fragment>
   );
 }
 
