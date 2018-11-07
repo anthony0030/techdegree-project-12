9675f823d22717ca1b45049615c4229880f4036d
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-07 12:54:06 +0200

    Refactor Framer
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Framer.js
+++ src/components/Framer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Button, Modal, ModalBody } from 'reactstrap';
 import Icon from "./Icon";
@@ -19,37 +19,30 @@ class Framer extends React.Component {
     this.closeModal  = this.closeModal.bind(this);
   }
 
-  toggleModal() {
-    this.setState({
-      modalOpen: !this.state.modalOpen
-    });
-  }
+  toggleModal= (event) => {
+    this.setState({ modalOpen: !this.state.modalOpen });
+  };
 
   openModal= (event) => {
-    this.setState({
-      modalOpen: true
-    });
-    console.log(this)
+    this.setState({ modalOpen: true });
     this.props.history.push(`/home/${this.props.title.replace(" ", "-")}`);
-  }
+  };
 
   closeModal = (event) => {
-    this.setState({
-      modalOpen: false
-    });
-    console.log(this)
+    this.setState({ modalOpen: false });
     this.props.history.push("/home");
-  }
+  };
 
   render() {
-    // console.log(this.props)
+    const {modalOpen} = this.state;
+    const {className, url, preTitle, title} = this.props;
     return (
       <React.Fragment>
         {/*Open Modal Button*/}
         <Button onClick={this.openModal} target="_blank" className="enlarge-icon">
           <Icon i="enlarge"/>
         </Button>
-        <Modal isOpen={this.state.modalOpen} toggle={this.toggleModal} className={this.props.className}>
+        <Modal isOpen={modalOpen} toggle={this.toggleModal} className={className}>
           <ModalBody>
             {/*Close Modal Button*/}
             <button onClick={this.closeModal} target="_blank" className="shrink-icon">
@@ -57,10 +50,11 @@ class Framer extends React.Component {
             </button>
             <h1 className="modal-title text-light bg-dark">
               <img src={logo} alt="logo" className="main-logo" />
-              <span className="d-none d-md-inline align-middle">{this.props.preTitle}</span>
-              <span className="align-middle"> {this.props.title}</span>
+              <span className="d-none d-md-inline align-middle">{preTitle}</span>
+              &nbsp;
+              <span className="align-middle">{title}</span>
             </h1>
-            <iframe frameBorder="0" className="viewer-iframe" tabIndex="0" title={this.props.title} src={this.props.url} />
+            <iframe frameBorder="0" className="viewer-iframe" tabIndex="0" title={title} src={url} />
             <Footer />
           </ModalBody>
         </Modal>
@@ -69,12 +63,13 @@ class Framer extends React.Component {
   }
 }
 
-
 Framer.propTypes = {
   title: PropTypes.string.isRequired,
+  preTitle: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
   projectIndex: PropTypes.number.isRequired,
   modalOpen: PropTypes.bool.isRequired
 };
 
-// export default Framer;
 export default withRouter(Framer);
