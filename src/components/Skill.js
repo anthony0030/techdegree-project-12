a89e423f9a6d31ce6cd1342eb8504cfe0213b8c7
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-07 11:30:34 +0200

    fix capitilazation of tooltip in skill component
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -83,13 +83,13 @@ class Skill extends Component {
 
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
+    this.toggleTooltip = this.toggleTooltip.bind(this);
     this.state = {
       tooltipOpen: false
     };
   }
 
-  toggle() {
+  toggleTooltip() {
     this.setState({
       tooltipOpen: !this.state.tooltipOpen
     });
@@ -98,92 +98,92 @@ class Skill extends Component {
   render() {
   const { skill, projectIndex, skillIndex } = this.props;
   const id = `id-${projectIndex}-${skillIndex}`;
-  let icon, toolTip;
+  let icon, tooltip;
   let alt = `${skill.toLowerCase().charAt(0).toUpperCase()} Badge Icon`
 
   switch(skill) {
-    case "bootstrap":    icon = bootstrap;    toolTip = "I used Bootstrap in this project!";           break;
-    case "bower":        icon = bower;        toolTip = "I used Bower in this project!";               break;
-    case "jquery":       icon = jquery;       toolTip = "I used jQuery in this project!";              break;
-    case "npm":          icon = npm;          toolTip = "I used npm in this project!";                 break;
-    case "android":      icon = android;      toolTip = "I used Android in this project!";             break;
-    case "angular":      icon = angular;      toolTip = "I used Angular in this project!";             break;
-    case "apache":       icon = apache;       toolTip = "I used Apache in this project!";              break;
-    case "appcelerator": icon = appcelerator; toolTip = "I used Appcelerator in this project!";        break;
-    case "apple":        icon = apple;        toolTip = "I used Apple in this project!";               break;
-    case "atom":         icon = atom;         toolTip = "I used Atom in this project!";                break;
-    case "aws":          icon = aws;          toolTip = "I used AWS in this project!";                 break;
-    case "babel":        icon = babel;        toolTip = "I used Babel in this project!";               break;
-    case "backbone":     icon = backbone;     toolTip = "I used Backbone in this project!";            break;
-    case "bitbucket":    icon = bitbucket;    toolTip = "I used Bitbucket in this project!";           break;
-    case "chrome":       icon = chrome;       toolTip = "I used Chrome in this project!";              break;
-    case "codeignitor":  icon = codeignitor;  toolTip = "I used Codeignitor in this project!";         break;
-    case "coffeescript": icon = coffeescript; toolTip = "I used Coffeescript in this project!";        break;
-    case "composer":     icon = composer;     toolTip = "I used Composer in this project!";            break;
-    case "cookie":       icon = cookie;       toolTip = "I used Cookies in this project!";             break;
-    case "css":          icon = css;          toolTip = "I used CSS in this project!";                 break;
-    case "cucumber":     icon = cucumber;     toolTip = "I used Cucumber in this project!";            break;
-    case "d3js":         icon = d3js;         toolTip = "I used D3js in this project!";                break;
-    case "debian":       icon = debian;       toolTip = "I used Debian in this project!";              break;
-    case "django":       icon = django;       toolTip = "I used Django in this project!";              break;
-    case "docker":       icon = docker;       toolTip = "I used Docker in this project!";              break;
-    case "drupal":       icon = drupal;       toolTip = "I used Drupal in this project!";              break;
-    case "firefox":      icon = firefox;      toolTip = "I used Firefox in this project!";             break;
-    case "gatling":      icon = gatling;      toolTip = "I used Gatling in this project!";             break;
-    case "gimp":         icon = gimp;         toolTip = "I used Gimp in this project!";                break;
-    case "git":          icon = git;          toolTip = "I used git in this project!";                 break;
-    case "github":       icon = github;       toolTip = "I used GitHub in this project!";              break;
-    case "gitlab":       icon = gitlab;       toolTip = "I used GitLab in this project!";              break;
-    case "gradle":       icon = gradle;       toolTip = "I used Gradle in this project!";              break;
-    case "grunt":        icon = grunt;        toolTip = "I used Grunt in this project!";               break;
-    case "gulp":         icon = gulp;         toolTip = "I used Gulp in this project!";                break;
-    case "html":         icon = html;         toolTip = "I used HTML in this project!";                break;
-    case "illustrator":  icon = illustrator;  toolTip = "I used Illustrator in this project!";         break;
-    case "inkscape":     icon = inkscape;     toolTip = "I used Inkscape in this project!";            break;
-    case "jade":         icon = jade;         toolTip = "I used JADE in this project!";                break;
-    case "java":         icon = java;         toolTip = "I used Java in this project!";                break;
-    case "js":           icon = js;           toolTip = "I used JavaScript in this project!";          break;
-    case "krakenjs":     icon = krakenjs;     toolTip = "I used KrakenJS in this project!";            break;
-    case "laravel":      icon = laravel;      toolTip = "I used Laravel in this project!";             break;
-    case "less":         icon = less;         toolTip = "I used LESS in this project!";                break;
-    case "linux":        icon = linux;        toolTip = "I used Linux in this project!";               break;
-    case "meteor":       icon = meteor;       toolTip = "I used Meteor in this project!";              break;
-    case "mocha":        icon = mocha;        toolTip = "I used Mocha in this project!";               break;
-    case "mongo":        icon = mongo;        toolTip = "I used Mongo in this project!";               break;
-    case "mysql":        icon = mysql;        toolTip = "I used MySQL in this project!";               break;
-    case "nginx":        icon = nginx;        toolTip = "I used NGINX in this project!";               break;
-    case "node":         icon = node;         toolTip = "I used Node in this project!";                break;
-    case "nodewebkit":   icon = nodewebkit;   toolTip = "I used Node Webkit in this project!";         break;
-    case "osx":          icon = osx;          toolTip = "I used OSX in this project!";                 break;
-    case "photoshop":    icon = photoshop;    toolTip = "I used Photoshop in this project!";           break;
-    case "php":          icon = php;          toolTip = "I used PHP in this project!";                 break;
-    case "phpstorm":     icon = phpstorm;     toolTip = "I used PHP Storm in this project!";           break;
-    case "postgres":     icon = postgres;     toolTip = "I used Postgres in this project!";            break;
-    case "protractor":   icon = protractor;   toolTip = "I used Protractor in this project!";          break;
-    case "python":       icon = python;       toolTip = "I used Python in this project!";              break;
-    case "rails":        icon = rails;        toolTip = "I used Rails in this project!";               break;
-    case "react":        icon = react;        toolTip = "I used REACT in this project!";               break;
-    case "sass":         icon = sass;         toolTip = "I used SASS in this project!";                break;
-    case "seqelize":     icon = seqelize;     toolTip = "I used Seqelize in this project!";            break;
-    case "ssh":          icon = ssh;          toolTip = "I used SSH in this project!";                 break;
-    case "swift":        icon = swift;        toolTip = "I used Swift in this project!";               break;
-    case "tomcat":       icon = tomcat;       toolTip = "I used Tomcat in this project!";              break;
-    case "ts":           icon = ts;           toolTip = "I used Type Script in this project!";         break;
-    case "ubuntu":       icon = ubuntu;       toolTip = "I used Ubuntu in this project!";              break;
-    case "vim":          icon = vim;          toolTip = "I used Vim in this project!";                 break;
-    case "visualstudio": icon = visualstudio; toolTip = "I used Visual Studio in this project!";       break;
-    case "windows":      icon = windows;      toolTip = "I used Windows in this project!";             break;
-    case "wordpress":    icon = wordpress;    toolTip = "I used Wordpress in this project!";           break;
-    case "api":          icon = api;          toolTip = "I used an API in this project!";              break;
-    case "svg":          icon = svg;          toolTip = "I used SVGs in this project!";                break;
-    case "error":        icon = error;        toolTip = "Error!";                                      break;
-    default:             icon = error;        toolTip = "Failed to load icon or icon does not exist";  break;
+    case "bootstrap":    icon = bootstrap;    tooltip = "I used Bootstrap in this project!";           break;
+    case "bower":        icon = bower;        tooltip = "I used Bower in this project!";               break;
+    case "jquery":       icon = jquery;       tooltip = "I used jQuery in this project!";              break;
+    case "npm":          icon = npm;          tooltip = "I used npm in this project!";                 break;
+    case "android":      icon = android;      tooltip = "I used Android in this project!";             break;
+    case "angular":      icon = angular;      tooltip = "I used Angular in this project!";             break;
+    case "apache":       icon = apache;       tooltip = "I used Apache in this project!";              break;
+    case "appcelerator": icon = appcelerator; tooltip = "I used Appcelerator in this project!";        break;
+    case "apple":        icon = apple;        tooltip = "I used Apple in this project!";               break;
+    case "atom":         icon = atom;         tooltip = "I used Atom in this project!";                break;
+    case "aws":          icon = aws;          tooltip = "I used AWS in this project!";                 break;
+    case "babel":        icon = babel;        tooltip = "I used Babel in this project!";               break;
+    case "backbone":     icon = backbone;     tooltip = "I used Backbone in this project!";            break;
+    case "bitbucket":    icon = bitbucket;    tooltip = "I used Bitbucket in this project!";           break;
+    case "chrome":       icon = chrome;       tooltip = "I used Chrome in this project!";              break;
+    case "codeignitor":  icon = codeignitor;  tooltip = "I used Codeignitor in this project!";         break;
+    case "coffeescript": icon = coffeescript; tooltip = "I used Coffeescript in this project!";        break;
+    case "composer":     icon = composer;     tooltip = "I used Composer in this project!";            break;
+    case "cookie":       icon = cookie;       tooltip = "I used Cookies in this project!";             break;
+    case "css":          icon = css;          tooltip = "I used CSS in this project!";                 break;
+    case "cucumber":     icon = cucumber;     tooltip = "I used Cucumber in this project!";            break;
+    case "d3js":         icon = d3js;         tooltip = "I used D3js in this project!";                break;
+    case "debian":       icon = debian;       tooltip = "I used Debian in this project!";              break;
+    case "django":       icon = django;       tooltip = "I used Django in this project!";              break;
+    case "docker":       icon = docker;       tooltip = "I used Docker in this project!";              break;
+    case "drupal":       icon = drupal;       tooltip = "I used Drupal in this project!";              break;
+    case "firefox":      icon = firefox;      tooltip = "I used Firefox in this project!";             break;
+    case "gatling":      icon = gatling;      tooltip = "I used Gatling in this project!";             break;
+    case "gimp":         icon = gimp;         tooltip = "I used Gimp in this project!";                break;
+    case "git":          icon = git;          tooltip = "I used git in this project!";                 break;
+    case "github":       icon = github;       tooltip = "I used GitHub in this project!";              break;
+    case "gitlab":       icon = gitlab;       tooltip = "I used GitLab in this project!";              break;
+    case "gradle":       icon = gradle;       tooltip = "I used Gradle in this project!";              break;
+    case "grunt":        icon = grunt;        tooltip = "I used Grunt in this project!";               break;
+    case "gulp":         icon = gulp;         tooltip = "I used Gulp in this project!";                break;
+    case "html":         icon = html;         tooltip = "I used HTML in this project!";                break;
+    case "illustrator":  icon = illustrator;  tooltip = "I used Illustrator in this project!";         break;
+    case "inkscape":     icon = inkscape;     tooltip = "I used Inkscape in this project!";            break;
+    case "jade":         icon = jade;         tooltip = "I used JADE in this project!";                break;
+    case "java":         icon = java;         tooltip = "I used Java in this project!";                break;
+    case "js":           icon = js;           tooltip = "I used JavaScript in this project!";          break;
+    case "krakenjs":     icon = krakenjs;     tooltip = "I used KrakenJS in this project!";            break;
+    case "laravel":      icon = laravel;      tooltip = "I used Laravel in this project!";             break;
+    case "less":         icon = less;         tooltip = "I used LESS in this project!";                break;
+    case "linux":        icon = linux;        tooltip = "I used Linux in this project!";               break;
+    case "meteor":       icon = meteor;       tooltip = "I used Meteor in this project!";              break;
+    case "mocha":        icon = mocha;        tooltip = "I used Mocha in this project!";               break;
+    case "mongo":        icon = mongo;        tooltip = "I used Mongo in this project!";               break;
+    case "mysql":        icon = mysql;        tooltip = "I used MySQL in this project!";               break;
+    case "nginx":        icon = nginx;        tooltip = "I used NGINX in this project!";               break;
+    case "node":         icon = node;         tooltip = "I used Node in this project!";                break;
+    case "nodewebkit":   icon = nodewebkit;   tooltip = "I used Node Webkit in this project!";         break;
+    case "osx":          icon = osx;          tooltip = "I used OSX in this project!";                 break;
+    case "photoshop":    icon = photoshop;    tooltip = "I used Photoshop in this project!";           break;
+    case "php":          icon = php;          tooltip = "I used PHP in this project!";                 break;
+    case "phpstorm":     icon = phpstorm;     tooltip = "I used PHP Storm in this project!";           break;
+    case "postgres":     icon = postgres;     tooltip = "I used Postgres in this project!";            break;
+    case "protractor":   icon = protractor;   tooltip = "I used Protractor in this project!";          break;
+    case "python":       icon = python;       tooltip = "I used Python in this project!";              break;
+    case "rails":        icon = rails;        tooltip = "I used Rails in this project!";               break;
+    case "react":        icon = react;        tooltip = "I used REACT in this project!";               break;
+    case "sass":         icon = sass;         tooltip = "I used SASS in this project!";                break;
+    case "seqelize":     icon = seqelize;     tooltip = "I used Seqelize in this project!";            break;
+    case "ssh":          icon = ssh;          tooltip = "I used SSH in this project!";                 break;
+    case "swift":        icon = swift;        tooltip = "I used Swift in this project!";               break;
+    case "tomcat":       icon = tomcat;       tooltip = "I used Tomcat in this project!";              break;
+    case "ts":           icon = ts;           tooltip = "I used Type Script in this project!";         break;
+    case "ubuntu":       icon = ubuntu;       tooltip = "I used Ubuntu in this project!";              break;
+    case "vim":          icon = vim;          tooltip = "I used Vim in this project!";                 break;
+    case "visualstudio": icon = visualstudio; tooltip = "I used Visual Studio in this project!";       break;
+    case "windows":      icon = windows;      tooltip = "I used Windows in this project!";             break;
+    case "wordpress":    icon = wordpress;    tooltip = "I used Wordpress in this project!";           break;
+    case "api":          icon = api;          tooltip = "I used an API in this project!";              break;
+    case "svg":          icon = svg;          tooltip = "I used SVGs in this project!";                break;
+    case "error":        icon = error;        tooltip = "Error!";                                      break;
+    default:             icon = error;        tooltip = "Failed to load icon or icon does not exist";  break;
   }
 
     return(
       <React.Fragment>
-        <Tooltip placement="top" isOpen={this.state.tooltipOpen} target={id} toggle={this.toggle}>
-          {toolTip}
+        <Tooltip placement="top" isOpen={this.state.tooltipOpen} target={id} toggle={this.toggleTooltip}>
+          {tooltip}
         </Tooltip>
         <div id={id}>
           <img src={icon} alt={alt} className="skill-badge" />
