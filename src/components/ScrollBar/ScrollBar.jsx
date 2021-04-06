ed6417d00d2814970cef8cf3a3ea7fa01e274ddd
Anthony Veaudry <anthony@veaudry.pro>
2021-04-06 13:40:37 +0300

    convert scroll bar to functional component
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/components/ScrollBar/ScrollBar.jsx
+++ src/components/ScrollBar/ScrollBar.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class ScrollBar extends Component {
+function ScrollBar() {
 
-  componentDidMount() {
+  useEffect(() => {
     window.onscroll = function() {
       var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
       var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       var scrolled = (winScroll / height) * 100;
       document.getElementById("myHorizontalBar").style.width = scrolled + "%";
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="scrollProgressBar__container fixed-top">
-        <div id="myHorizontalBar" className="scrollProgressBar__progress bg-secondary"></div>
-      </div>
-    );
-  }
+
+  return (
+    <div className="scrollProgressBar__container fixed-top">
+      <div id="myHorizontalBar" className="scrollProgressBar__progress bg-secondary"></div>
+    </div>
+  )
 }
 
-export default ScrollBar;
+export default ScrollBar
