302aba09a91d6227e765da19b9edd6f7783f3ebc
Anthony Veaudry <anthony@veaudry.pro>
2021-04-07 18:18:57 +0300

    adds info to the about section
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,14 +1,55 @@
 import React from 'react'
-import { Heading } from "components";
+import { Heading, Skills } from "components";
 
 function About() {
   return (
     <>
       <Heading title="" subtitle="About" />
-      <ul>
-        <li>Your CV/Resume or descriptive information about yourself and your UX design background.</li>
-        <li>This page also contains a skills section.</li>
-      </ul>
+
+      <h3>Resume</h3>
+
+      <div className="timeline mb-5">
+        <div className="timeline-container left">
+          <div className="content">
+            <h2>2017 - current</h2>
+            <p>Go Thassos</p>
+          </div>
+        </div>
+        <div className="timeline-container right">
+          <div className="content">
+            <h2>2016 - 2017</h2>
+            <p>TEI EMT</p>
+          </div>
+        </div>
+        <div className="timeline-container left">
+          <div className="content">
+            <h2>2014 - 2016</h2>
+            <p>EPAL</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <h3>Skills</h3>
+      <div className="bg-light rounded border">
+        <Skills
+        projectIndex={0}
+        skills={[
+          "html",
+          "css",
+          "js",
+          "sass",
+          "scss",
+          "react",
+          "pug",
+          "jade",
+          "bower",
+          "npm",
+          "api",
+          "git"
+        ]} />
+      </div>
     </>
   )
 }
