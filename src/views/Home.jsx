a798c21db72ffdf55aef424b8327562189b42b3f
Anthony Veaudry <anthony@veaudry.pro>
2021-04-07 18:20:59 +0300

    adds some info to the home page
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,10 +5,13 @@ function Home() {
   return (
     <>
      <Heading title="" subtitle="Home" />
-      <ul>
-        <li>One line header/descriptor</li>
-        <li>Some information about you</li>
-      </ul>
+      <h3>Welcome to my online Portfolio!</h3>
+      <p>
+        My name is Anthony. I am a Web developer. I enjoy deploying computer networks. I also enjoy setting up PBX systems.
+      </p>
+      <p>
+        More information comming soon!
+      </p>
     </>
   )
 }
