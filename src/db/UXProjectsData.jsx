e3c0d0c2e8c433b23de0289b4d100946b789e46e
Anthony Veaudry <anthony@veaudry.pro>
2021-04-09 19:24:56 +0300

    adds project data
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/db/UXProjectsData.jsx
+++ src/db/UXProjectsData.jsx
@@ -1,24 +1,66 @@
 // Application Images
 
-import AppPhoto01 from "../assets/images/UXPhotos/01.png";
-import AppPhoto02 from "../assets/images/UXPhotos/02.png";
-import AppPhoto03 from "../assets/images/UXPhotos/03.png";
+import AppPhoto01 from "../assets/images/UXPhotos/shirts-by-mike/preview.png";
+import AppPhoto02 from "../assets/images/UXPhotos/grocery-app/preview.png";
+import AppPhoto03 from "../assets/images/UXPhotos/whooshmail/preview.png";
 
 const PROJECTS = [
   {
-    id: "1",
+    id: "shirts-by-mike",
     title: "Shirts by Mike",
     photo: AppPhoto01,
     description: "<p>In this case study I critiqued their site and came up with areas of improvement.</p>",
     overview: "",
     problem: "<p>I was tasked with critiquing <a href='shirtsbymike.com'>shirtsbymike.com</a>, an e-commerce website that sells t-shirts. My 'client' wanted to know if anything should be changed to make the site look and/or function better for their customers. I described the design elements I saw on the site and determined what improvements needed to be made. I Explained each improvement so your they understood how it will benefit their customers.</p>",
     audience: "<div class='text-dark bg-light rounded border p-5 mb-3'><i class='fas fa-user fa-5x float-right'></i>Mostly ages 16-35 <br />Primarily based in the United States <br />Treehouse students <br />Lifelong learners <br />Values community <br />Playful <br /></div>",
-    solution: "",
-    learnings: "",
-    photos: "",
+    solution: "<p></p>",
+    learnings: "<p>In this Case study I leaned how to make an Empathy map and A journey map. I also improved my schetching and wireframing. I was also maid aware of the concept of mood boards. Finlay got a better grasp of using personas in design.</p>",
+    galleries: [
+      {
+        title: "Research",
+        images: [
+          "/assets/images/UXPhotos/shirts-by-mike/research/Empathy Map.jpg",
+          "/assets/images/UXPhotos/shirts-by-mike/research/Journey Map.jpg"
+        ]
+      },
+      {
+        title: "Sketches",
+        images: [
+          "/assets/images/UXPhotos/shirts-by-mike/sketches/Desktop 1.jpg",
+          "/assets/images/UXPhotos/shirts-by-mike/sketches/Desktop 2.jpg",
+          "/assets/images/UXPhotos/shirts-by-mike/sketches/Mobile.jpg"
+        ]
+      },
+      {
+        title: "Wireframes",
+        images: [
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Desktop - Catalog.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Desktop - Contact.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Desktop - Home.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Desktop - Product.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Mobile - Catalog.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Mobile - Contact.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Mobile - Home.png",
+          "/assets/images/UXPhotos/shirts-by-mike/wireframes/Mobile - Product.png"
+        ]
+      },
+      {
+        title: "Mockups",
+        images: [
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Desktop - Catalog.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Desktop - Contact.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Desktop - Home.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Desktop - Product.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Mobile - Catalog.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Mobile - Contact.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Mobile - Home.png",
+          "/assets/images/UXPhotos/shirts-by-mike/mockups/Mobile - Product.png"
+        ]
+      }
+    ],
   },
   {
-    id: "2",
+    id: "grocery-app",
     title: "Grocery App",
     photo: AppPhoto02,
     description: "In this case study I added a parking Finder to a grocery app",
@@ -27,10 +69,10 @@ const PROJECTS = [
     audience: "",
     solution: "",
     learnings: "",
-    photos: "",
+    galleries: [],
   },
   {
-    id: "3",
+    id: "whooshmail",
     title: "WhooshMail",
     photo: AppPhoto03,
     description: "In this case study I designed and refined two workflows for the mailing app Wooshmail",
@@ -39,7 +81,7 @@ const PROJECTS = [
     audience: "",
     solution: "",
     learnings: "",
-    photos: "",
+    galleries: [],
   }
 ];
 
