8bf47e45a4951a309682c33d4d5bd3978b3f6964
Anthony Veaudry <anthony@veaudry.pro>
2021-04-09 12:35:00 +0300

    work on home page
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Heading } from "components";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const buttonClass = "btn btn-lg btn-primary m-5 p-5"
+
   return (
     <>
      <Heading title="Home" subtitle="" />
@@ -9,9 +12,19 @@ function Home() {
       <p>
         My name is Anthony. I am a Web developer. I enjoy deploying computer networks. I also enjoy setting up PBX systems.
       </p>
-      <p>
-        More information comming soon!
-      </p>
+
+      <div className="d-flex justify-content-center">
+        <Link className={buttonClass} to="/fewd">
+          FEWD
+          <br />
+          PORTFOLIO
+        </Link>
+        <Link className={buttonClass} to="/ux">
+          UX
+          <br />
+          PORTFOLIO
+        </Link>
+      </div>
     </>
   )
 }
