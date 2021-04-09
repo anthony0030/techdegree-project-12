a38131ba2d458422942511300adad50fa1e4b8ba
Anthony Veaudry <anthony@veaudry.pro>
2021-04-09 12:34:36 +0300

    adds some data to ux projects
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/db/UXProjectsData.jsx
+++ src/db/UXProjectsData.jsx
@@ -9,19 +9,37 @@ const PROJECTS = [
     id: "1",
     title: "Shirts by Mike",
     photo: AppPhoto01,
-    description: "In this case study I critiqued their site and came up with areas of improvement."
+    description: "<p>In this case study I critiqued their site and came up with areas of improvement.</p>",
+    overview: "",
+    problem: "<p>I was tasked with critiquing <a href='shirtsbymike.com'>shirtsbymike.com</a>, an e-commerce website that sells t-shirts. My 'client' wanted to know if anything should be changed to make the site look and/or function better for their customers. I described the design elements I saw on the site and determined what improvements needed to be made. I Explained each improvement so your they understood how it will benefit their customers.</p>",
+    audience: "<div class='text-dark bg-light rounded border p-5 mb-3'><i class='fas fa-user fa-5x float-right'></i>Mostly ages 16-35 <br />Primarily based in the United States <br />Treehouse students <br />Lifelong learners <br />Values community <br />Playful <br /></div>",
+    solution: "",
+    learnings: "",
+    photos: "",
   },
   {
     id: "2",
     title: "Grocery App",
     photo: AppPhoto02,
-    description: "In this case study I added a parking Finder to a grocery app"
+    description: "In this case study I added a parking Finder to a grocery app",
+    overview: "",
+    problem: "",
+    audience: "",
+    solution: "",
+    learnings: "",
+    photos: "",
   },
   {
     id: "3",
     title: "WhooshMail",
     photo: AppPhoto03,
-    description: "In this case study I designed and refined two workflows for the mailing app Wooshmail"
+    description: "In this case study I designed and refined two workflows for the mailing app Wooshmail",
+    overview: "",
+    problem: "",
+    audience: "",
+    solution: "",
+    learnings: "",
+    photos: "",
   }
 ];
 
