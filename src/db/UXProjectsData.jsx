d8bd09752c94b76bfacf6dded328c4723a94a81d
Anthony Veaudry <anthony@veaudry.pro>
2021-04-11 23:33:52 +0300

    adds project data
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/db/UXProjectsData.jsx
+++ src/db/UXProjectsData.jsx
@@ -1,14 +1,8 @@
-// Application Images
-
-import AppPhoto01 from "../assets/images/UXPhotos/shirts-by-mike/preview.png";
-import AppPhoto02 from "../assets/images/UXPhotos/grocery-app/preview.png";
-import AppPhoto03 from "../assets/images/UXPhotos/whooshmail/preview.png";
-
 const PROJECTS = [
   {
     id: "shirts-by-mike",
     title: "Shirts by Mike",
-    photo: AppPhoto01,
+    photo: "/assets/images/UXPhotos/shirts-by-mike/preview.png",
     overview: "<p>In this case study I critiqued their site and came up with areas of improvement.</p>",
     problem: "<p>I was tasked with critiquing <a href='shirtsbymike.com'>shirtsbymike.com</a>, an e-commerce website that sells t-shirts. My 'client' wanted to know if anything should be changed to make the site look and/or function better for their customers. I described the design elements I saw on the site and determined what improvements needed to be made. I Explained each improvement so your they understood how it will benefit their customers.</p>",
     audience: "<div class='text-dark bg-light rounded border p-5 mb-3'><i class='fas fa-user fa-5x float-right'></i>Mostly ages 16-35 <br />Primarily based in the United States <br />Treehouse students <br />Lifelong learners <br />Values community <br />Playful <br /></div>",
@@ -160,24 +154,332 @@ const PROJECTS = [
   {
     id: "grocery-app",
     title: "Grocery App",
-    photo: AppPhoto02,
+    photo: "/assets/images/UXPhotos/grocery-app/preview.png",
     overview: "In this case study I added a parking Finder to a grocery app",
-    problem: "",
-    audience: "",
-    solution: "",
-    learnings: "",
-    galleries: [],
+    problem: "In this case study I was tasked to find the pain points of shoppers",
+    audience: "<div class='text-dark bg-light rounded border p-5 mb-3'><i class='fas fa-user fa-5x float-right'></i>Anyone that needs to shop from a store.<br />People that are unable to go to stores.<br />These people Include disabled people, People with difficulty moving, and people stuck in quarantine due to a pandemic.</div>",
+    solution: "I identified that parking can be a problem at times to shoppers. I developed a screen for shoppers to see if there will be parking at there store.",
+    learnings: "I learned how to conduct interviews, and process the information that came from them. I also learned how to make device mockups. I also improved my page maps.",
+    galleries: [
+      {
+        title: "Mockups",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Extra Sketches.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Extra Sketches.png",
+            caption: "Sketches",
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Loading Sketches.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Loading Sketches.png",
+            caption: "Sketches"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Sketches.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/sketches/Project 5 Sketches.png",
+            caption: "Sketches"
+          }
+        ],
+      },
+      {
+        title: "Mockups",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/1. Parking Finder – loading.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/1. Parking Finder – loading.png",
+            caption: "Loading Mockup",
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/1. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/1. Parking Finder.png",
+            caption: "Search Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/2. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/2. Parking Finder.png",
+            caption: "Results Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/3. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/3. Parking Finder.png",
+            caption: "Notification Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/4. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/4. Parking Finder.png",
+            caption: "No Results Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/5. Sign-In.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/5. Sign-In.png",
+            caption: "Sign-In Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/mockups/6. Sign-up.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/mockups/6. Sign-up.png",
+            caption: "Sign-Up Mockup"
+          }
+        ],
+      },
+      {
+        title: "Device Mockups",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/1. Parking Finder – loading.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/1. Parking Finder – loading.png",
+            caption: "Loading Mockup",
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/1. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/1. Parking Finder.png",
+            caption: "Search Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/2. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/2. Parking Finder.png",
+            caption: "Results Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/3. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/3. Parking Finder.png",
+            caption: "Notification Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/4. Parking Finder.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/4. Parking Finder.png",
+            caption: "No Results Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/5. Sign-In.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/5. Sign-In.png",
+            caption: "Sign-In Mockup"
+          },
+          {
+            src: "/assets/images/UXPhotos/grocery-app/device mockups/6. Sign-up.png",
+            thumbnail: "/assets/images/UXPhotos/grocery-app/device mockups/6. Sign-up.png",
+            caption: "Sign-Up Mockup"
+          }
+        ],
+      }
+    ]
   },
   {
     id: "whooshmail",
     title: "WhooshMail",
-    photo: AppPhoto03,
+    photo: "/assets/images/UXPhotos/whooshmail/preview.png",
     overview: "In this case study I designed and refined two workflows for the mailing app Wooshmail",
-    problem: "",
-    audience: "",
-    solution: "",
-    learnings: "",
-    galleries: [],
+    problem: "I needed to make two flows. One for basic users. One for admin users. Each flow would have differences depending on the role of the user.",
+    audience: `<div class='text-dark bg-light rounded border p-5 mb-3'>
+                <i class='fas fa-user fa-5x float-right'></i>
+                <b>Admins</b> <br />
+                Mostly ages 40-60 <br />
+                Primarily based in the United States <br />
+                Small business owners <br />
+                High School/GED or Bachelor’s Degree <br />
+                Ready to be their own boss <br />
+                Pursuing their passions <br />
+                Used their personal funds to get started <br />
+                <br />
+                <b>Basic:</b> <br />
+                Mostly ages 20-40 <br />
+                Primarily based in the United States <br />
+                Work for a small business <br />
+                High School/GED <br />
+              </div>`,
+    solution: "I started this project by making two personas, one for each user role. I then made story boards. After that I made some sketches. Wire frames followed. Combining them to make two prototypes",
+    learnings: "In this project I leaned how to use google slides. I also improved my storyboarding.",
+    galleries: [
+      {
+        title: "Personas",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/whooshmail/personas/Admin Persona.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/personas/Admin Persona.png",
+            caption: "Admin Persona",
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/personas/Basic Persona.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/personas/Basic Persona.png",
+            caption: "Basic Persona"
+          }
+        ],
+      },
+      {
+        title: "Story Boards",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Story Boards/Story Board - Admin.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Story Boards/Story Board - Admin.png",
+            caption: "Admin Story Board",
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Story Boards/Story Board - Basic.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Story Boards/Story Board - Basic.png",
+            caption: "Basic Story Board"
+          }
+        ],
+      },
+      {
+        title: "Sketches",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Add Recipients Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Add Recipients Page 1.jpg",
+            caption: "Admin Add Recipients Page 1",
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Add Recipients Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Add Recipients Page 2.jpg",
+            caption: "Admin Add Recipients Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Campaign Analytics Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Campaign Analytics Page 0.jpg",
+            caption: "Admin Campaign Analytics Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Campaign Analytics Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Campaign Analytics Page 2.jpg",
+            caption: "Admin Campaign Analytics Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Edit Email Template Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Edit Email Template Page 1.jpg",
+            caption: "Admin Edit Email Template Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Edit Email Template Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Edit Email Template Page 2.jpg",
+            caption: "Admin Edit Email Template Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Email Template Gallery Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Email Template Gallery Page 1.jpg",
+            caption: "Admin Email Template Gallery Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Email Template Gallery Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Email Template Gallery Page 2.jpg",
+            caption: "Admin Email Template Gallery Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Send Email Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Send Email Page 1.jpg",
+            caption: "Admin Send Email Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Send Email Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Admin Send Email Page 2.jpg",
+            caption: "Admin Send Email Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Add Recipients Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Add Recipients Page 1.jpg",
+            caption: "Basic Add Recipients Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Add Recipients Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Add Recipients Page 2.jpg",
+            caption: "Basic Add Recipients Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Campaign Analytics Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Campaign Analytics Page 1.jpg",
+            caption: "Basic Campaign Analytics Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Campaign Analytics Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Campaign Analytics Page 2.jpg",
+            caption: "Basic Campaign Analytics Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Edit Email Template Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Edit Email Template Page 1.jpg",
+            caption: "Basic Edit Email Template Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Edit Email Template Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Edit Email Template Page 2.jpg",
+            caption: "Basic Edit Email Template Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Email Template Gallery Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Email Template Gallery Page 1.jpg",
+            caption: "Basic Email Template Gallery Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Email Template Gallery Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Email Template Gallery Page 2.jpg",
+            caption: "Basic Email Template Gallery Page 2"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Send Email Page 1.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Send Email Page 1.jpg",
+            caption: "Basic Send Email Page 1"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Send Email Page 2.jpg",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Sketches/Basic Send Email Page 2.jpg",
+            caption: "Basic Send Email Page 2"
+          },
+        ],
+      },
+      {
+        title: "Wireframes",
+        images: [
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Add Recipients Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Add Recipients Page.png",
+            caption: "Admin Add Recipients Page",
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Campaign Analytics Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Campaign Analytics Page.png",
+            caption: "Admin Campaign Analytics Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Edit Email Template Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Edit Email Template Page.png",
+            caption: "Admin Edit Email Template Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Email Template Gallery Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Email Template Gallery Page.png",
+            caption: "Admin Email Template Gallery Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Send Email Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Admin Send Email Page.png",
+            caption: "Admin Send Email Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Add Recipients Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Add Recipients Page.png",
+            caption: "Basic Add Recipients Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Campaign Analytics Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Campaign Analytics Page.png",
+            caption: "Basic Campaign Analytics Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Edit Email Template Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Edit Email Template Page.png",
+            caption: "Basic Edit Email Template Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Email Template Gallery Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Email Template Gallery Page.png",
+            caption: "Basic Email Template Gallery Page"
+          },
+          {
+            src: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Send Email Page.png",
+            thumbnail: "/assets/images/UXPhotos/whooshmail/Wireframes/Basic Send Email Page.png",
+            caption: "Basic Send Email Page"
+          },
+        ],
+      },
+    ],
   }
 ];
 
