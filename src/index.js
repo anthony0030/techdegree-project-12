7a21af75395e653b77190efaff6cf4d20064b57d
Anthony Veaudry <anthony@veaudry.pro>
2021-04-13 13:36:43 +0300

    adds loggs
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ if (module.hot) {
   console.info('✅  Server-side HMR Enabled!');
 }
 
+console.log("process.env.PORT", process.env.PORT)
+console.log("process.env", process.env)
+
 const port = process.env.PORT || 3000;
 
 export default express()
